5c47e7b63843294e349f0c3176709083062579fc
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-08-02 22:07:45 +0530

    Added calculator project

---
+++ calculator.py
@@ -0,0 +1,26 @@
+# EXERCISE :Create a calculate capable of performing additon , subtraction , multiplication , and division operations on two numbers .
+
+a = int (input ("enter number : "))
+b = int (input ("enter number : "))
+
+addition = a + b
+print ("Addition of number is : ",addition ) 
+
+subtraction = a-b
+print ("Subtraction of number is : ",subtraction )
+
+multiplication = a*b
+print ("Multiplication of number is :  ",multiplication)
+
+division = a/b
+print ("Division of number is :  ",division)
+
+# OUTPUT :
+"""
+enter number : 4                                          # user input 
+enter number : 8                                          # user input 
+Addition of number is  12
+Subtraction of number is  -4
+Multiplication of number is  32
+Division of number is  0.5 
+"""
